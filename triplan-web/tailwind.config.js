0c470b3f62ee5651ef9dd04035e657440470d56f
Pipp432 <pipp432@gmail.com>
2023-01-20 21:43:28 +0700

    Created Next.js Defualt app and typescript, TW CSS

--- triplan-web/tailwind.config.js
+++ triplan-web/tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-​
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			animation: {
+				"fade-in": "fade 0.3s ease-in",
+			},
+			keyframes: {
+				fade: {
+					"0%": { opacity: 0 },
+					"100%": { opacity: 1 },
+				},
+			},
+		},
+	},
+	plugins: [],
+};
