faf5c0fedc76e425aaf542c04a0fbcdc8118f214
search <search35453@gmail.com>
2023-01-24 01:01:08 +0700

    Change index styling

--- triplan-web/pages/index.tsx
+++ triplan-web/pages/index.tsx
@@ -1,8 +1,18 @@
 import classes from "../styles/Home.module.css";
+
 export default function Home() {
-	return (
-		<div className='grid place-items-center h-screen '>
-			<div className={classes.glow}>Hello TriPlan</div>
-		</div>
-	);
+    return (
+        <div className="flex h-screen bg-white pt-24 text-black">
+            <div className="m-auto w-80 text-center">
+                <h1 className="text-3xl font-bold p-5">TRIPLAN</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Culpa unde perferendis optio dolorem officia ex iure
+                    assumenda exercitationem, delectus facilis ad inventore
+                    quasi accusamus fugiat? Distinctio similique incidunt
+                    provident deleniti.
+                </p>
+            </div>
+        </div>
+    );
 }
