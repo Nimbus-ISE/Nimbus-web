bd535c75dc164ea53f7b8f8c04fd4caeaa03a27d
search <search35453@gmail.com>
2023-01-24 07:48:33 +0700

    Style changes

--- triplan-web/pages/index.tsx
+++ triplan-web/pages/index.tsx
@@ -4,27 +4,34 @@ import Card from "../components/Card";
 import { places } from "@/public/data";
 
 export default function Home() {
-	return (
-    <>
-      <Head>
-        <title>TriPlan</title>
-      </Head>
-      <main className='grid place-items-center h-screen '>
-        <div className={classes.glow}>Hello TriPlan</div>
-        <div className="flex">          
-          {
-            places.map((place: any) => (
-              <Card 
-                location={place.placeName}
-                city={place.city}
-                link={place.link}
-                image={place.image}
-                tags={place.tags}
-              />
-            ))
-          }
-        </div>
-      </main>
-    </>
-	);
+    return (
+        <>
+            <Head>
+                <title>TriPlan</title>
+            </Head>
+            <main className="grid place-items-center h-full pt-24 bg-neutral-100 text-black">
+                <div className="w-80 m-auto text-center">
+                    <h1 className="text-4xl font-bold p-3">TRIPLAN</h1>
+                    <p className="p-3">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Nostrum omnis consequatur maxime accusamus ea
+                        voluptates sequi delectus quasi voluptatibus modi
+                        aliquid, cum harum iusto soluta iste rem? Voluptates,
+                        deleniti voluptatum.
+                    </p>
+                </div>
+                <div className="flex">
+                    {places.map((place: any) => (
+                        <Card
+                            location={place.placeName}
+                            city={place.city}
+                            link={place.link}
+                            image={place.image}
+                            tags={place.tags}
+                        />
+                    ))}
+                </div>
+            </main>
+        </>
+    );
 }
