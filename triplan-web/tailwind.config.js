4153f007b866e0f4274c3279cbe1f855e69281ef
search <search35453@gmail.com>
2023-01-26 22:54:46 +0700

    Update tailwind config

--- triplan-web/tailwind.config.js
+++ triplan-web/tailwind.config.js
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			animation: {
-				"fade-in": "fade 0.3s ease-in",
-			},
-			keyframes: {
-				fade: {
-					"0%": { opacity: 0 },
-					"100%": { opacity: 1 },
-				},
-			},
-      scale: {
-        '102': '1.02',
-      },
-		},
-	},
-	plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            animation: {
+                "fade-in": "fade 0.3s ease-in",
+            },
+            keyframes: {
+                fade: {
+                    "0%": { opacity: 0 },
+                    "100%": { opacity: 1 },
+                },
+            },
+            scale: {
+                102: "1.02",
+            },
+            colors: {
+                tricolorgreen: "#00c4cc",
+            },
+        },
+    },
+    plugins: [],
 };
