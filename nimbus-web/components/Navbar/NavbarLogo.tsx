1c1bb4592ac92af19452e3c727806bab6abbf5b7
search <search35453@gmail.com>
2023-01-30 09:47:48 +0700

    fix: Fixed logo not loading in production

--- nimbus-web/components/Navbar/NavbarLogo.tsx
+++ nimbus-web/components/Navbar/NavbarLogo.tsx
@@ -11,7 +11,7 @@ const NavbarLogo = () => {
         >
             <Image
                 className="w-[40px] h-[30px]"
-                src={"/images/logo.PNG"}
+                src={"/images/logo.png"}
                 alt="Logo"
                 width={40}
                 height={30}
