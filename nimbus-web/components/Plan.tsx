c413d9f8315be38de48653cebaf10499920c51bb
search <search35453@gmail.com>
2023-02-01 18:12:17 +0700

    feat: Created a plan component to integrate page indicator and form

---
+++ nimbus-web/components/Plan.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Form from "@/components/Form/Form";
+import PageIndicator from "@/components/PageIndicator/PageIndicator";
+import useScrollPercentage from "@/hooks/useScrollPercentage";
+
+const list = [
+    { name: "Location" },
+    { name: "Dates" },
+    { name: "Style" },
+    { name: "Budget" },
+    { name: "Tags" },
+];
+
+const Plan = () => {
+    return (
+        <div className="flex">
+            <PageIndicator list={list} />
+            <div
+                id="form-container"
+                className="overflow-y-scroll rounded-xl h-[20rem] w-fit mx-auto"
+            >
+                {list.map((item) => {
+                    return (
+                        <div id="input-container">
+                            <Form />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Plan;
