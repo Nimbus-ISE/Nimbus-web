94d5e64b496e574b86d942795ed335676a7f0192
search <search35453@gmail.com>
2023-02-01 18:12:59 +0700

    feat: Created a plan route to render Plan component

---
+++ nimbus-web/pages/plan.tsx
@@ -0,0 +1,10 @@
+import Form from "@/components/Form/Form";
+import PageIndicator from "@/components/PageIndicator/PageIndicator";
+import Plan from "@/components/Plan";
+import React from "react";
+
+const plan = () => {
+    return <Plan />;
+};
+
+export default plan;
