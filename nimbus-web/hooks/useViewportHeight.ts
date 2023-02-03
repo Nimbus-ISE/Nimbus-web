ed5f6bed5b42553e0b45ec8a4f87042ead82a54a
search <search35453@gmail.com>
2023-02-03 11:19:06 +0700

    fix: fix useViewportHeight hook

--- nimbus-web/hooks/useViewportHeight.ts
+++ nimbus-web/hooks/useViewportHeight.ts
@@ -2,13 +2,18 @@ import React from "react";
 
 const useViewportHeight = () => {
     //a custom hook is required to get the real viewport height that is offset by navbar
-    const [height, setHeight] = React.useState<number>(64);
+    const [height, setHeight] = React.useState<number>(window.innerHeight - 64);
     React.useEffect(() => {
-        const navbarElement = document.getElementById("navbar");
-        if (navbarElement) setHeight(navbarElement?.offsetHeight);
+        window.addEventListener("resize", () =>
+            setHeight(window.innerHeight - 64)
+        );
+        return () =>
+            window.removeEventListener("resize", () =>
+                setHeight(window.innerHeight - 64)
+            );
     }, []);
     return {
-        height: window.innerHeight - height,
+        height: height,
     };
 };
 
