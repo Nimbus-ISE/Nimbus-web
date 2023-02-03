582889abd855328a71f00d09c90cdb86d8a759f5
Chwnsn <chawin.sung@gmail.com>
2023-02-03 09:45:01 +0700

    override datepicker styling

--- nimbus-web/components/Forms/DateForm.tsx
+++ nimbus-web/components/Forms/DateForm.tsx
@@ -7,18 +7,27 @@ import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const DateForm = (props: any) => {
-    const [value, setValue] = React.useState<Dayjs | null>(null);
+    const [startValue, setStartValue] = React.useState<Dayjs | null>(null);
+    const [endValue, setEndValue] = React.useState<Dayjs | null>(null);
     return (
-        <div className="flex flex-wrap justify-around mx-auto">
+        <div className="flex flex-wrap justify-evenly mx-auto">
             <div className="start-date my-2">
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <LocalizationProvider dateAdapter={AdapterDayjs} injectFirst>
                     <DatePicker
-                        label="Start Date"
-                        value={value}
+                        value={startValue}
                         onChange={(newValue) => {
-                            setValue(newValue);
+                            setStartValue(newValue);
                         }}
-                        renderInput={(params) => <TextField {...params} />}
+                        label={startValue === null ? "start date" : ""}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                className="bg-gray-100 appearance-none border-0 border-gray-100 rounded-xl py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:bg-white hover:opacity-70"
+                                sx={{ borderStyle: "none" }}
+                                InputLabelProps={{ shrink: false }}
+                            />
+                        )}
+                        className="bg-gray-100 appearance-none border-2 border-gray-100 rounded-xl py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:bg-white hover:opacity-70"
                     />
                 </LocalizationProvider>
             </div>
@@ -26,9 +35,9 @@ const DateForm = (props: any) => {
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DatePicker
                         label="End Date"
-                        value={value}
+                        value={endValue}
                         onChange={(newValue) => {
-                            setValue(newValue);
+                            setEndValue(newValue);
                         }}
                         renderInput={(params) => <TextField {...params} />}
                     />
