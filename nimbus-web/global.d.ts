50dc73dbf4cef3623393d744538f89171b5e60ad
search <search35453@gmail.com>
2023-03-26 22:38:34 +0700

    refactor: Added template types for form data

--- nimbus-web/global.d.ts
+++ nimbus-web/global.d.ts
@@ -4,3 +4,9 @@ declare interface IForm {
     type: string;
     title: string;
 }
+declare interface IKeyString {
+    [key: string]: any;
+}
+declare interface IFormData extends IKeyString {
+    // TODO: add types to send to ALGO here
+}
