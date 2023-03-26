c6012a912c3699dcc61bd97bdd842c14a0a04ad2
search <search35453@gmail.com>
2023-03-26 14:39:51 +0700

    feat: Implemented search bar and tags button

---
+++ nimbus-web/components/Search/TagsButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const TagsButton = () => {
+    const router = useRouter();
+    const handleOnClick = () => {
+        router.push("/search");
+    };
+    return (
+        <button
+            onClick={handleOnClick}
+            className="bg-neutral-100 h-8 my-auto md:border-[1px] duration-300
+            shadow-md md:shadow-sm rounded-full px-3 py-1 text-sm hover:bg-neutral-200 transition"
+        >
+            Tags
+        </button>
+    );
+};
+
+export default TagsButton;
