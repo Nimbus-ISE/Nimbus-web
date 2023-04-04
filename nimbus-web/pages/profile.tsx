760d9f204ecb349c19ba3ee322c1da478d50f22a
search <search35453@gmail.com>
2023-04-04 22:52:06 +0700

    feat: Render string date instead of number date

--- nimbus-web/pages/profile.tsx
+++ nimbus-web/pages/profile.tsx
@@ -4,13 +4,16 @@ import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import Loading from "../components/Loading";
 import ErrorMessage from "../components/ErrorMessage";
 import UserProfile from "@/components/UserProfile";
+import getPremiumType from "@/utils/getPremiumType";
+import getPremiumExpire from "@/utils/getPremiumExpire";
 
 function Profile() {
     const { user, isLoading } = useUser();
-    //const isLoading = false;
+    const [date, setDate] = React.useState<string>();
     React.useEffect(() => {
         if (user) {
-            console.log(user);
+            const expireDate = getPremiumExpire(user) as any;
+            setDate(new Date(expireDate).toString());
         }
     }, [user]);
     return (
@@ -22,8 +25,8 @@ function Profile() {
                         <UserProfile src={user.picture as string} size={40} />
                         <h2>{user.name}</h2>
                         <p className="">{user.email}</p>
-                        <div>Status: {user?.premium as any} </div>
-                        <div>Expire: {user?.premium_expire as any} </div>
+                        <div>Status: {getPremiumType(user) as any} </div>
+                        <div>Expire: {date} </div>
                     </div>
                 </div>
             ) : null}
