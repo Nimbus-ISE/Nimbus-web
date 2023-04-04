5ebf089709f842171897b11eff4f488cb2127cb1
Pipp432 <pipp432@gmail.com>
2023-04-04 20:59:34 +0700

    Update getLocationData

--- nimbus-web/pages/api/getLocationData.ts
+++ nimbus-web/pages/api/getLocationData.ts
@@ -6,17 +6,16 @@ export default async function handler(
     res: NextApiResponse
 ) {
     const loc_ids: any = req.query.loc_ids;
+    console.log(loc_ids);
+
     const arr: any = JSON.parse(loc_ids);
     try {
         const location_data =
             await sql`SELECT L.loc_id, L.loc_name, L.description, L.province, 
-        L.rating AS location_rating,L.lat,L.lng, R.review_text, 
-        R.author, R.rating AS review_rating, R.review_date, I.url
-        FROM location_data L, review R, image I
-        WHERE L.loc_id = R.loc_id AND R.loc_id = I.loc_id AND L.loc_id IN ${sql(
-            [arr]
-        )}
-        GROUP BY L.loc_id, R.review_text, R.author, R.review_date, R.rating, I.url`;
+        L.rating AS location_rating,L.lat,L.lng, I.url
+        FROM location_data L,  image I
+        WHERE L.loc_id = I.loc_id AND L.loc_id IN ${sql([arr])}
+        GROUP BY L.loc_id, I.url`;
         console.log(location_data);
 
         res.status(200).json(location_data);
