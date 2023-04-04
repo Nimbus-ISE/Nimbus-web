08e5f84d533cc88c5abc3a6a8fd443e29f38027e
Pipp432 <pipp432@gmail.com>
2023-04-04 20:32:10 +0700

    Update getLocationData query and fixed mult-loc

--- nimbus-web/pages/api/getLocationData.ts
+++ nimbus-web/pages/api/getLocationData.ts
@@ -5,14 +5,17 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const loc_id = req.query.loc_id;
+    const loc_ids: any = req.query.loc_ids;
+    const arr: any = JSON.parse(loc_ids);
     try {
         const location_data =
             await sql`SELECT L.loc_id, L.loc_name, L.description, L.province, 
-        L.rating AS location_rating, R.review_text, 
+        L.rating AS location_rating,L.lat,L.lng, R.review_text, 
         R.author, R.rating AS review_rating, R.review_date, I.url
         FROM location_data L, review R, image I
-        WHERE L.loc_id = R.loc_id AND R.loc_id = I.loc_id AND L.loc_id IN (${loc_id})
+        WHERE L.loc_id = R.loc_id AND R.loc_id = I.loc_id AND L.loc_id IN ${sql(
+            [arr]
+        )}
         GROUP BY L.loc_id, R.review_text, R.author, R.review_date, R.rating, I.url`;
         console.log(location_data);
 
