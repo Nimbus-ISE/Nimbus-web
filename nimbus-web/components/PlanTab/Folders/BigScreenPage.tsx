dba45c059355fa9ce6eab9ed4036cab9c76dd134
Pipp432 <pipp432@gmail.com>
2023-04-05 19:45:11 +0700

    Fixed big screen error

--- nimbus-web/components/PlanTab/Folders/BigScreenPage.tsx
+++ nimbus-web/components/PlanTab/Folders/BigScreenPage.tsx
@@ -64,79 +64,79 @@ const BigScreenPage = () => {
                     <SideBar />
                     {openSavePlan && <SavePlanPopUp />}
 
-                    {!openFullTab && fullPlan && (
-                    //     <Map
-                    //         ref={mapRef}
-                    //         initialViewState={{
-                    //             latitude: 13.7563,
-                    //             longitude: 100.5018,
-                    //             zoom: 15,
-                    //         }}
-                    //         attributionControl={false}
-                    //         style={{
-                    //             gridColumnStart: 5,
-                    //             gridColumnEnd: "span 12",
-                    //         }}
-                    //         mapboxAccessToken={
-                    //             "pk.eyJ1IjoicGlwcC00MzIiLCJhIjoiY2xkYnF1NXU4MDM2MjNxcXdrczFibHJsdiJ9.uuksf9mguzejH6e6R0RQxg"
-                    //         }
-                    //         mapStyle="mapbox://styles/mapbox/streets-v12?optimize=true'"
-                    //     >
-                    //         {fullPlan[currentFolder] &&
-                    //             fullPlan[currentFolder].map(
-                    //                 (point: any, index: any) => {
-                    //                     return (
-                    //                         <Marker
-                    //                             longitude={point.lng}
-                    //                             latitude={point.lat}
-                    //                             anchor="bottom"
-                    //                             onClick={(e) => {
-                    //                                 console.log(index);
+                    {/* {!openFullTab && fullPlan && (
+                        <Map
+                            ref={mapRef}
+                            initialViewState={{
+                                latitude: 13.7563,
+                                longitude: 100.5018,
+                                zoom: 15,
+                            }}
+                            attributionControl={false}
+                            style={{
+                                gridColumnStart: 5,
+                                gridColumnEnd: "span 12",
+                            }}
+                            mapboxAccessToken={
+                                "pk.eyJ1IjoicGlwcC00MzIiLCJhIjoiY2xkYnF1NXU4MDM2MjNxcXdrczFibHJsdiJ9.uuksf9mguzejH6e6R0RQxg"
+                            }
+                            mapStyle="mapbox://styles/mapbox/streets-v12?optimize=true'"
+                        >
+                            {fullPlan[currentFolder] &&
+                                fullPlan[currentFolder].map(
+                                    (point: any, index: any) => {
+                                        return (
+                                            <Marker
+                                                longitude={point.lng}
+                                                latitude={point.lat}
+                                                anchor="bottom"
+                                                onClick={(e) => {
+                                                    console.log(index);
 
-                    //                                 togglePinState(
-                    //                                     currentFolder,
-                    //                                     index
-                    //                                 );
-                    //                                 onSelect(
-                    //                                     point.lng,
-                    //                                     point.lat
-                    //                                 );
-                    //                                 e.originalEvent.stopPropagation();
-                    //                             }}
-                    //                         >
-                    //                             <span className="text-center bg-black text-cyan-300 p-[2px] rounded">
-                    //                                 {
-                    //                                     fullPlan[currentFolder][
-                    //                                         index
-                    //                                     ].loc_name
-                    //                                 }
-                    //                             </span>
-                    //                             {pinState[currentFolder] && (
-                    //                                 <Pin
-                    //                                     fill={
-                    //                                         pinState[
-                    //                                             currentFolder
-                    //                                         ][index]
-                    //                                     }
-                    //                                     number={index + 1}
-                    //                                 />
-                    //                             )}
-                    //                         </Marker>
-                    //                     );
-                    //                 },
-                    //                 []
-                    //             )}
+                                                    togglePinState(
+                                                        currentFolder,
+                                                        index
+                                                    );
+                                                    onSelect(
+                                                        point.lng,
+                                                        point.lat
+                                                    );
+                                                    e.originalEvent.stopPropagation();
+                                                }}
+                                            >
+                                                <span className="text-center bg-black text-cyan-300 p-[2px] rounded">
+                                                    {
+                                                        fullPlan[currentFolder][
+                                                            index
+                                                        ].loc_name
+                                                    }
+                                                </span>
+                                                {pinState[currentFolder] && (
+                                                    <Pin
+                                                        fill={
+                                                            pinState[
+                                                                currentFolder
+                                                            ][index]
+                                                        }
+                                                        number={index + 1}
+                                                    />
+                                                )}
+                                            </Marker>
+                                        );
+                                    },
+                                    []
+                                )}
 
-                    //         <ScaleControl position="top-right" />
-                    //         <AttributionControl
-                    //             position="top-left"
-                    //             compact={true}
-                    //         />
-                    //         <Source id="my-data" type="geojson" data={geojson}>
-                    //             <Layer {...layerStyle} />
-                    //         </Source>
-                    //     </Map>
-                    // )}
+                            <ScaleControl position="top-right" />
+                            <AttributionControl
+                                position="top-left"
+                                compact={true}
+                            />
+                            <Source id="my-data" type="geojson" data={geojson}>
+                                <Layer {...layerStyle} />
+                            </Source>
+                        </Map>
+                    )} */}
 
                     <div className="col-span-8 w-full h-[100%]">
                         {openReview && (
