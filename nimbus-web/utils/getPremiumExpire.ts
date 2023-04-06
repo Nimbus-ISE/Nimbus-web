4337c7f0d6e9a1f9dd124b62df46ad737e76f3bc
search <search35453@gmail.com>
2023-04-06 18:42:45 +0700

    refactor: Added type to get date as number and default to be date object

--- nimbus-web/utils/getPremiumExpire.ts
+++ nimbus-web/utils/getPremiumExpire.ts
@@ -1,7 +1,12 @@
 import { UserProfile } from "@auth0/nextjs-auth0/client";
 
-const getPremiumExpire = (user: UserProfile) => {
-    return user[`${window.location.origin}/premium_expire`] as number;
+const getPremiumExpire = (user: UserProfile, type?: "number") => {
+    const numDate = user[`${window.location.origin}/premium_expire`] as number;
+    if (type === undefined) {
+        return new Date(numDate);
+    } else {
+        return numDate;
+    }
 };
 
 export default getPremiumExpire;
