c3546b7c4c0ef63e59b0d5104d7b78ae3b0184bf
search <search35453@gmail.com>
2023-04-09 01:22:16 +0700

    feat: Modified getLocation util to include address,lat,lng,tags

--- nimbus-web/utils/api/getLocation.ts
+++ nimbus-web/utils/api/getLocation.ts
@@ -3,9 +3,13 @@ import sql from "@/postgres";
 interface ILocationItemData extends IReview {
     loc_id: number;
     loc_name: string;
+    address: string;
+    lat: string;
+    lng: string;
     description: string;
     province: string;
     location_rating: string;
+    tags: string;
     url: string;
 }
 
@@ -13,19 +17,28 @@ const getLocation = async (loc_id: string) => {
     return new Promise(async (resolve, reject) => {
         try {
             const location_data = await sql`
-            SELECT L.loc_id, L.loc_name, L.description, L.province, 
+            SELECT L.loc_id, L.loc_name, L.description, L.province, L.address, L.lat, L.lng,
             L.rating AS location_rating, R.review_text, 
-            R.author, R.rating AS review_rating, R.review_date, R.url AS review_url, I.url
-            FROM location_data L, review R, image I
-            WHERE L.loc_id = R.loc_id AND R.loc_id = I.loc_id AND L.loc_id IN (${loc_id})
-            GROUP BY L.loc_id, R.review_text, R.author, R.review_date, R.rating, R.url, I.url`;
+            R.author, R.rating AS review_rating, R.review_date, R.url AS review_url, I.url,
+            string_agg(B.tag_name, ', ') as full_tag_list
+            FROM location_data L 
+            JOIN review R ON L.loc_id = R.loc_id 
+            JOIN image I ON R.loc_id = I.loc_id 
+            JOIN belong_to B ON L.loc_id = B.loc_id
+            WHERE L.loc_id = ${loc_id}
+            GROUP BY L.loc_id, R.review_text, R.author, R.review_date, R.rating, R.url, I.url
+            `;
             const generalLocationInfo = {
                 loc_id: location_data[0].loc_id,
                 loc_name: location_data[0].loc_name,
+                address: location_data[0].address,
+                lat: location_data[0].lat,
+                lng: location_data[0].lng,
                 description: location_data[0].description,
                 province: location_data[0].province,
                 location_rating: location_data[0].location_rating,
-                url: location_data[0].url,
+                tags: location_data[0].full_tag_list,
+                url: [location_data[0].url],
             };
             console.log("GENERAL DATA", generalLocationInfo);
             const reviews = location_data.map((location: ILocationItemData) => {
