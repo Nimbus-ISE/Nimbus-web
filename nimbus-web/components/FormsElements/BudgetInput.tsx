aa9d485ab0fa6f679f9faf68adbcb6bd6ace8f31
Chwnsn <chawin.sung@gmail.com>
2023-04-09 20:22:15 +0700

    update budget input

--- nimbus-web/components/FormsElements/BudgetInput.tsx
+++ nimbus-web/components/FormsElements/BudgetInput.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { NumericFormat } from "react-number-format";
 import { PlanContext } from "../Plan";
+import Slider, { SliderValueLabelProps } from "@mui/material/Slider";
+import Tooltip from "@mui/material/Tooltip";
+import { ThemeProvider } from "@mui/material";
+import { nimbusTheme } from "../../styles/NimbusMuiTheme";
 
-const budgetClassName =
-    "w-full text-l h-14 bg-gray-100 shadow-md appearance-none border-2 border-gray-100 rounded-xl py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:bg-gray-100 hover:opacity-70 max-w-[30rem]";
-const budgetValid = "budget-input " + budgetClassName;
-const budgetInvalid = "budget-input-invalid " + budgetClassName;
+import AttachMoneyRoundedIcon from "@mui/icons-material/AttachMoneyRounded";
+import MoneyOffCsredRoundedIcon from "@mui/icons-material/MoneyOffCsredRounded";
 
-const BudgetInput = () => {
+function ValueLabelComponent(props: SliderValueLabelProps) {
+    const { children, value } = props;
+
+    return (
+        <Tooltip enterTouchDelay={30} placement="top" title={value}>
+            {children}
+        </Tooltip>
+    );
+}
+
+function valueMapper(value: number) {
+    if (value === 0) return <MoneyOffCsredRoundedIcon fontSize="small" />;
+    return [...Array(value)].map(() => (
+        <AttachMoneyRoundedIcon fontSize="small" />
+    ));
+}
+
+const BudgetInput = (props: any) => {
     const { formData, setFormDataField } = React.useContext(PlanContext);
-    const [value, setValue] = React.useState<number>();
+    const [value, setValue] = React.useState<number>(0);
     const [isValid, setIsValid] = React.useState<boolean>(true);
 
     React.useEffect(() => {
@@ -24,6 +43,11 @@ const BudgetInput = () => {
         setValue(num);
     };
 
+    const handleChange = (event: any, newValue: number | number[]) => {
+        console.log(event.target.value);
+        setValue(event.target.value);
+    };
+
     const handleKeyDown = (e: any) => {
         if (e.key === "Enter") {
             e.preventDefault();
@@ -35,17 +59,46 @@ const BudgetInput = () => {
             <NumericFormat
                 defaultValue={formData.budget}
                 onValueChange={handleValueChange}
-                autoFocus={false}
-                thousandSeparator={true}
-                decimalScale={0}
-                className={isValid ? budgetValid : budgetInvalid}
-                placeholder="Enter your budget"
-                allowNegative={false}
                 onKeyDown={handleKeyDown}
             />
             <span className={isValid ? "help-block" : "help-block-invalid"}>
                 Please enter a budget between 100 and 1,000,000 Baht.
             </span>
+
+            <ThemeProvider theme={nimbusTheme}>
+                <div className="flex justify-center mx-auto items-center h-11/12 w-40 ">
+                    <Slider
+                        onChange={handleChange}
+                        slots={{
+                            valueLabel: ValueLabelComponent,
+                        }}
+                        defaultValue={formData.budget}
+                        valueLabelDisplay="on"
+                        step={1}
+                        min={0}
+                        max={4}
+                        color="secondary"
+                        marks
+                        valueLabelFormat={(value) => {
+                            return (
+                                <div style={{ textAlign: "center" }}>
+                                    {value === 0
+                                        ? "no cost"
+                                        : value === 1
+                                        ? "below 100 THB"
+                                        : value === 2
+                                        ? "below 500 THB"
+                                        : value === 3
+                                        ? "below 1000 THB"
+                                        : "above 1000 THB"}
+                                    <br />
+                                    {valueMapper(value)}
+                                </div>
+                            );
+                        }}
+                    />
+                </div>
+            </ThemeProvider>
         </>
     );
 };
