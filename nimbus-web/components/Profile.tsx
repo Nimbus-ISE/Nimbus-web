f28e438a63f54eac810c2b20cfe1fea791ac9eb3
search <search35453@gmail.com>
2023-04-16 18:12:52 +0700

    feat: Added saved plans and recently viewed with mock data in profile page

--- nimbus-web/components/Profile.tsx
+++ nimbus-web/components/Profile.tsx
@@ -3,22 +3,46 @@ import { UserProfile as UserProfileType } from "@auth0/nextjs-auth0/client";
 import getPremiumExpire from "@/utils/getPremiumExpire";
 import getPremiumType from "@/utils/getPremiumType";
 import UserProfile from "./UserProfile";
+import Slider from "./SliderList/Slider";
+import PlanCard from "./Cards/PlanCard";
 
-const Profile = ({ user }: { user: UserProfileType }) => {
+interface IProps {
+    user: UserProfileType;
+    recentlyViewedList: Array<unknown>;
+    planList: Array<unknown>;
+}
+
+const Profile = ({ user, planList, recentlyViewedList }: IProps) => {
     const [date, setDate] = React.useState<string>();
+    const premiumExpire = getPremiumExpire(user);
     React.useEffect(() => {
-        if (user) {
-            setDate(getPremiumExpire(user).toString());
+        if (user && premiumExpire) {
+            setDate(premiumExpire.toString());
         }
     }, [user]);
     return user ? (
-        <div className="grid place-items-center min-h-screen h-full pt-24 bg-neutral-100 text-black">
-            <div>
+        <div className="grid grid-cols-1 min-h-screen h-full pt-24 bg-neutral-100 text-black">
+            <div className="mx-auto">
                 <UserProfile src={user.picture as string} size={40} />
                 <h2>{user.name}</h2>
                 <p className="">{user.email}</p>
                 <div>Status: {getPremiumType(user) as any} </div>
                 <div>Expire: {date} </div>
+            </div>
+            <PlanCard planList={planList} />
+            <div className="my-7 w-full">
+                <div
+                    className="md:w-[80%] mx-auto text-xl font-semibold text-neutral-500
+                 border-b-neutral-500 border-b-[1px] pb-1"
+                >
+                    RECENTLY VIEWED
+                </div>
+                <Slider
+                    title=" "
+                    locationList={recentlyViewedList}
+                    shape="circle"
+                    onClickCallback={(id) => {}}
+                />
             </div>
         </div>
     ) : null;
--- nimbus-web/pages/profile.tsx
+++ nimbus-web/pages/profile.tsx
@@ -3,11 +3,28 @@ import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import Loading from "../components/Loading";
 import ErrorMessage from "../components/ErrorMessage";
 import Profile from "@/components/Profile";
+import getTrendingList from "@/utils/api/getTrendingList";
+import getLocationList from "@/utils/api/getLocationList";
 
-function profile() {
+interface IProps {
+    recentlyViewedList: Array<unknown>;
+    planList: Array<unknown>;
+}
+
+function profile({ recentlyViewedList, planList }: IProps) {
     const { user, isLoading } = useUser();
     return (
-        <>{isLoading ? <Loading /> : user ? <Profile user={user} /> : null}</>
+        <>
+            {isLoading ? (
+                <Loading />
+            ) : user ? (
+                <Profile
+                    user={user}
+                    planList={planList}
+                    recentlyViewedList={recentlyViewedList}
+                />
+            ) : null}
+        </>
     );
 }
 
@@ -15,3 +32,10 @@ export default withPageAuthRequired(profile, {
     onRedirecting: () => <Loading />,
     onError: (error) => <ErrorMessage>{error.message}</ErrorMessage>,
 });
+
+export async function getStaticProps() {
+    const recentlyViewedList = await getTrendingList();
+    const planList = await getLocationList();
+    console.log(recentlyViewedList);
+    return { props: { recentlyViewedList, planList } };
+}
