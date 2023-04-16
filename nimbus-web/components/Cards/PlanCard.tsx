72549e2de87571179a023d817bc7ed720ebfa3c8
search <search35453@gmail.com>
2023-04-16 18:09:23 +0700

    feat: Update plan card

--- nimbus-web/components/Cards/PlanCard.tsx
+++ nimbus-web/components/Cards/PlanCard.tsx
@@ -1,25 +1,68 @@
 import Card from "@/components/Cards/Card";
-import CardVertical from "@/components/Cards/CardVertical";
-import { places } from "@/public/data";
+import React from "react";
+import useGridColumns from "@/hooks/useGridColumns";
+import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
+import ExpandLessRoundedIcon from "@mui/icons-material/ExpandLessRounded";
 
-const PlanCard = (props: any) => {
+interface IProps {
+    planList: Array<unknown>;
+}
+
+const PlanCard = ({ planList }: IProps) => {
+    const [expand, setExpand] = React.useState<boolean>(false);
+    const [cols, setCols] = React.useState<number>(0);
+    const gridColumnsClass = useGridColumns("plan-card-container");
+
+    const handleOnClick = () => {
+        setExpand((prev) => !prev);
+    };
+
+    React.useEffect(() => {
+        if (gridColumnsClass) setCols(Number(gridColumnsClass.split("-")[2]));
+    }, [gridColumnsClass]);
     return (
-        <>
-            <div className="flex flex-wrap justify-center ">
-                {places.map((place: any, index: number) => (
-                    <div className="scale-75" key={index}>
-                        <CardVertical
-                            location={place.placeName}
-                            city={place.city}
-                            link={place.link}
-                            image={place.image}
-                            tags={place.tags}
-                            key={index}
-                        />
-                    </div>
-                ))}
+        <div
+            id="plan-card-container"
+            className="flex flex-col md:w-[80%] mx-auto"
+        >
+            <div
+                className="text-xl font-semibold text-neutral-500
+                 border-b-neutral-500 border-b-[1px] pb-1"
+            >
+                SAVED PLANS
             </div>
-        </>
+            <div
+                className={`grid gap-5 ${gridColumnsClass} w-fit mx-auto my-5`}
+            >
+                {!expand && planList.length > cols
+                    ? [...planList].splice(0, cols).map((location: any) => (
+                          <div key={location.loc_id}>
+                              <Card location={location} />
+                          </div>
+                      ))
+                    : planList.map((location: any) => (
+                          <div key={location.loc_id}>
+                              <Card location={location} />
+                          </div>
+                      ))}
+            </div>
+            {planList.length > cols ? (
+                <button
+                    onClick={handleOnClick}
+                    className="text-tricolorgreen mx-auto w-52"
+                >
+                    {expand ? (
+                        <div>
+                            Close <ExpandLessRoundedIcon />
+                        </div>
+                    ) : (
+                        <div>
+                            Expand <ExpandMoreRoundedIcon />
+                        </div>
+                    )}
+                </button>
+            ) : null}
+        </div>
     );
 };
 
