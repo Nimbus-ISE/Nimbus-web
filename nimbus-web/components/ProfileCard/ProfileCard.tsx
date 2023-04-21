34bcdcaf43555f66a8d80ab67c1d209b5ce4c9bb
search <search35453@gmail.com>
2023-04-21 14:39:09 +0700

    style: Update ProfileCard styling

--- nimbus-web/components/ProfileCard/ProfileCard.tsx
+++ nimbus-web/components/ProfileCard/ProfileCard.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import { Button, Stack, Typography } from "@mui/material";
 import UserProfile from "../UserProfile";
+import { UserProfile as IUserProfile } from "@auth0/nextjs-auth0/client";
+import getUserEmail from "@/utils/getUserEmail";
+import getPremiumType from "@/utils/getPremiumType";
+import getPremiumExpire from "@/utils/getPremiumExpire";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCrown } from "@fortawesome/free-solid-svg-icons";
 
-
-interface ProfileCardProps {
-    data: { image: string; name: string; createAt: string };
-}
-
-const ProfileCard = ({ data }: ProfileCardProps) => {
-    return (
+const ProfileCard = ({ user }: { user: IUserProfile }) => {
+    const email = getUserEmail(user);
+    const premium_type = getPremiumType(user);
+    const premium_expire = getPremiumExpire(user) as Date;
+    return user ? (
         <Stack spacing={3} alignItems="center" direction="row">
-            <UserProfile src={data.image} size={80} />
+            <UserProfile src={user.picture as string} size={80} />
             <Stack
                 direction="row"
                 alignItems="center"
@@ -19,24 +23,54 @@ const ProfileCard = ({ data }: ProfileCardProps) => {
                 width="100%"
             >
                 <Stack>
-                    <Typography
-                        variant="h6"
-                        sx={{
-                            fontWeight: "bold",
-                        }}
-                    >
-                        {data.name}
-                    </Typography>
+                    <div className="flex gap-3">
+                        <Typography
+                            variant="h6"
+                            sx={{
+                                fontFamily: "montserrat",
+                                color: "#333333",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            {user.name ? user.name : email}
+                        </Typography>
+                        {premium_type !== "None" ? (
+                            <FontAwesomeIcon
+                                className="my-auto text-yellow-400 drop-shadow-sm"
+                                icon={faCrown}
+                            />
+                        ) : null}
+                    </div>
                     <Typography
                         variant="body2"
                         sx={{
-                            color: "#A6A6A6",
+                            fontFamily: "montserrat",
+                            color: "#696969",
                         }}
                     >
-                        ACCOUNT CREATED {data.createAt}
+                        {user.name ? (
+                            <>
+                                <span className="font-semibold">Email: </span>
+                                {email}
+                            </>
+                        ) : null}
                     </Typography>
+                    {premium_type !== "None" ? (
+                        <Typography
+                            variant="body2"
+                            sx={{
+                                fontFamily: "montserrat",
+                                color: "#696969",
+                            }}
+                        >
+                            <span className="font-semibold">
+                                Premium Expires:
+                            </span>{" "}
+                            {premium_expire.toDateString()}
+                        </Typography>
+                    ) : null}
                 </Stack>
-                <Button
+                {/*<Button
                     variant="contained"
                     sx={{
                         backgroundColor: "#45D8D0",
@@ -49,16 +83,17 @@ const ProfileCard = ({ data }: ProfileCardProps) => {
                         align="center"
                         sx={{
                             color: "#FFFFFF",
+                            fontFamily: "montserrat",
                             fontWeight: "bold",
                         }}
                     >
                         EDIT
                     </Typography>
                     <EditIcon sx={{ fontSize: 14 }} />
-                </Button>
+                </Button>*/}
             </Stack>
         </Stack>
-    );
+    ) : null;
 };
 
 export default ProfileCard;
--- nimbus-web/stories/ProfileCard.stories.tsx
+++ nimbus-web/stories/ProfileCard.stories.tsx
@@ -1,6 +1,4 @@
-import FullDetailCard from "@/components/FullDetailCard/FullDetailCard";
 import ProfileCard from "@/components/ProfileCard/ProfileCard";
-import Profile from "@/pages/profile";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 export default {
@@ -15,9 +13,17 @@ const Template: ComponentStory<typeof ProfileCard> = (args) => (
 export const RealDataExample = Template.bind({});
 
 RealDataExample.args = {
-    data: {
-        image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2022%2F11%2F22%2Flove-is-blind-bartise-112222-3.jpg",
-        name: "Chicken Soup",
-        createAt: "01/01/2023",
+    user: {
+        family_name: "P",
+        given_name: "Nattakit",
+        locale: "en",
+        name: "Nattakit P",
+        nickname: "search35453",
+        picture:
+            "https://lh3.googleusercontent.com/a/AEdFTp5a3gVM3PRFVmKChA74uW-FS210zmx4Lf7DAWwl4w=s96-c",
+        updated_at: "2023-01-24T01:01:39.063Z",
+        "https://nimbus-ise.vercel.app/premium_type": "None",
+        "https://nimbus-ise.vercel.app/premium_expire": 1682047709922,
+        "https://nimbus-ise.vercel.app/email": "hello123@gmail.com",
     },
 };
