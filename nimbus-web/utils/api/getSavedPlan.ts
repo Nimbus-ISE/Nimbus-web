29fb7215723f3a6f257e27e522789d05215f0e52
search <search35453@gmail.com>
2023-04-22 18:46:39 +0700

    feat: Updated query to include plan_id for delete saved plan

--- nimbus-web/utils/api/getSavedPlan.ts
+++ nimbus-web/utils/api/getSavedPlan.ts
@@ -5,13 +5,14 @@ const getSavedPlan = async (user_id: string | undefined) => {
         if (!user_id) reject();
         try {
             const savedPlanData = await sql`
-            SELECT plan_sequence FROM plan where plan_id in (
+            SELECT plan_id, plan_sequence FROM plan where plan_id in (
                 SELECT plan_id FROM user_data U JOIN saved S 
                  ON U.user_id = S.user_id
                  WHERE U.user_id = ${user_id})`;
             console.log("SAVEDPLANDATA", savedPlanData);
             const mapped = savedPlanData.map((planData: any) => {
                 const temp = JSON.parse(planData.plan_sequence)[0];
+                temp.plan_id = planData.plan_id;
                 return temp;
             });
             console.log("MAPPED", mapped);
