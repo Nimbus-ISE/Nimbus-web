23da8cd89a6043a9d3cf25a8eb5845bd0891c470
Pipp432 <pipp432@gmail.com>
2023-04-23 15:49:29 +0700

    Fixed getSavePlan

--- nimbus-web/utils/api/getSavedPlan.ts
+++ nimbus-web/utils/api/getSavedPlan.ts
@@ -2,13 +2,13 @@ import sql from "@/postgres";
 
 const getSavedPlan = async (user_id: string | undefined) => {
     return new Promise(async (resolve, reject) => {
-        // if (!user_id) reject();
+        if (!user_id) reject();
         try {
             const savedPlanData = await sql`
             SELECT plan_id, plan_sequence FROM plan where plan_id in (
                 SELECT plan_id FROM user_data U JOIN saved S 
                  ON U.user_id = S.user_id
-                 WHERE U.user_id = 'auth0|642c62dc9e6ad19131004860')`;
+                 WHERE U.user_id = ${user_id})`;
             console.log("SAVEDPLANDATA", savedPlanData);
             const mapped = savedPlanData.map((planData: any) => {
                 const temp = JSON.parse(planData.plan_sequence)[0];
