c3bea0945f5021d54abfa0e88a8ee41b54dfeb21
Nattakit Pisitsup <search35453@gmail.com>
2023-04-24 22:01:22 +0700

    perf: Added connection timeout to sql instances

--- nimbus-web/postgres.ts
+++ nimbus-web/postgres.ts
@@ -2,6 +2,8 @@ import postgres from "postgres";
 
 const sql: any = postgres(process.env.DB_CONN as string, {
     ssl: "require",
+    idle_timeout: 60,
+    max_lifetime: 60 * 30,
 });
 
 export default sql;
