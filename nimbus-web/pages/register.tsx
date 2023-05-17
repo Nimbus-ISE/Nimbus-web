6136b9790f78353c6e99a369806ca5671b206e38
Nattakit Pisitsup <search35453@gmail.com>
2023-05-17 13:27:03 +0700

    feat: Linked register route to google form

--- nimbus-web/pages/register.tsx
+++ nimbus-web/pages/register.tsx
@@ -1,7 +1,15 @@
+import Loading from "@/components/Loading";
 import React from "react";
 
 const register = () => {
-    return <div>register</div>;
+    React.useLayoutEffect(() => {
+        window.location.href = "https://forms.gle/bX5Jggdxi4gBfLn79";
+    });
+    return (
+        <div className="relative flex flex-col min-h-screen h-full w-screen overflow-x-hidden bg-neutral-100 text-neutral-800">
+            <Loading />
+        </div>
+    );
 };
 
 export default register;
